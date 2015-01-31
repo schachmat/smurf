625e9ed808ea1740253f6845f63289c8cd5fef27
Markus Teich <markus.teich@stusta.mhn.de>
2015-01-31 19:29:04 +0100

    stuff

--- bla.c
+++
@@ -1,7 +0,0 @@
-#define __attribute__(x) 
-#include "include/capi/cef_app_capi.h"
-
-int main(int argc, char** argv)
-{
-	return 0;
-}
--- implement_interface.py
+++ implement_interface.py
@@ -1,7 +1,7 @@
 #!/bin/env python
 
 import sys
-from pycparser import parse_file
+from pycparser import parse_file, c_generator
 
 sys.path.extend(['.', '..'])
 
@@ -18,5 +18,13 @@ if __name__ == "__main__":
     if len(sys.argv) != 2:
         sys.exit(1)
 
-    ast = parse_file(sys.argv[1], use_cpp=True, cpp_args=[r'-Ifake', r'-I.'])
-    ast.show(showcoord=True)
+    fname = sys.argv[1]
+    ast = parse_file("gen.c", use_cpp=True, cpp_args=[r'-Ifake', r'-I.'])
+    for e in ast.ext:
+        if not fname in str(e.coord):
+            continue
+
+        generator = c_generator.CGenerator()
+        print(generator.visit(e))
+
+#        e.show()
