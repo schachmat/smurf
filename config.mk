06ce28dd5b2f683cbfbc490febd5c83d59d2b972
Kai Arnold <kai.arnold@ericsson.com>
2015-10-21 16:58:00 -0700

    trim out more build options

--- config.mk
+++ config.mk
@@ -1,14 +1,13 @@
 # Customize below to fit your system
 
 # includes and libs
-INCS = -I. -I/usr/X11/include
-LIBS = -L. -lcef -lc -lX11
+INCS = -I.
+LIBS = -L. -lcef -lX11
 
 # flags
-CFLAGS = -std=c99 -Wall -O0 -g -pthread ${INCS}
-#CFLAGS = -std=c99 -pedantic -Wall -Os ${INCS}
+CFLAGS = -DDebug -std=c99 -Wall -O0 -g -pthread ${INCS}
 DLDFLAGS = -g -Wl,-rpath,. -pthread -Lbin/debug ${LIBS}
 RLDFLAGS = -s -Wl,-rpath,. -pthread -Lbin/release ${LIBS}
 
 # compiler and linker
-CC = cc
+CC = gcc
